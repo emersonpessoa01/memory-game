7acd15837b82ca4f0efbea2b5a2a47429406f878
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-12-28 20:58:48 -0300

    finished project :rocket:

---
+++ README.md
@@ -0,0 +1,10 @@
+## <p align="center">Game Memory</p>
+
+### Entrega do Desafio sugerido pela DIO : Potência Tech iFood - Desenvolvimento de Jogos
+
+ [Clique aqui]() para acessar o resultado final do jogo criada a partir do desafio!
+
+<p align="center">
+  <img src="./src/images/readme.png" alt="JSGame Detona Ralph Logo" width="100%">
+</p>
+
--- src/script/engine.js
+++ src/script/engine.js
@@ -1,18 +1,18 @@
 const emojis = [
-  "😱",
-  "😱",
+  "🐱",
+  "🐱",
   "🦝",
   "🦝",
-  "🐯",
-  "🐯",
+  "🦊",
+  "🦊",
   "🐶",
   "🐶",
   "🐵",
   "🐵",
   "🦁",
   "🦁",
-  "🦊",
-  "🦊",
+  "🐯",
+  "🐯",
   "🐮",
   "🐮",
 ];
@@ -24,11 +24,42 @@ for (let i = 0; i < emojis.length; i++) {
   let box = document.createElement("div");
   box.className = "item";
   box.innerHTML = shuffleEmojis[i];
+  box.onclick = handleClick;
   document.querySelector(".game").appendChild(box);
 }
-// (function reStart() {
-//   document.querySelector(".reset").addEventListener("click", (e) => {
-//     e.preventDefault();
-//     window.location.reload();
-//   });
-// })();
+
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
+
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  }
+
+  console.log(openCards);
+}
+
+function checkMatch() {
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  } else {
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  }
+
+  openCards = [];
+
+  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+    alert("Você venceu !");
+  }
+}
+
+(function reStart() {
+  document.querySelector(".reset").addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.reload();
+  });
+})();
